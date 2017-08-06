83dbc9d8efb610992edf5236a841e9b450ceb93a
Luke-JV <lukevoigt0407@gmail.com>
2017-08-06 17:04:19 +0200

    Model propagation from final DB
    Also initial setup and Early stages of VECTOR

---
+++ DocuPath/DocuPath/DBLayer/VECTOR.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DocuPath.DBLayer
+{
+    public static class VECTOR
+    {
+        
+        #region CAPSULE
+        //public static string CAPSULE(string key, string value)
+        //{
+        //    switch (key)
+        //    {
+        //        case "PASS":
+        //            {
+        //                if (VPass(value))
+        //                {
+        //                    return "TRUE";
+        //                }                    
+        //            }
+        //            break;
+        //        default:
+        //            {
+        //                return "999";
+        //            }
+                    
+        //    }
+        //    return "998";
+        //}
+        #endregion
+
+        #region ENCRYPTION
+
+        #endregion
+
+        #region AUTHENTICATION
+        public static bool VPass(string user,string pass)
+        {
+            DocuPathEntities db = new DocuPathEntities();
+            foreach (var UL in db.USER_LOGIN)
+            {
+                if (UL.Username == user && UL.Password == pass)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }                
+            }
+            return false;
+        }
+        #endregion
+
+        #region RAT GENERATION
+
+        #endregion
+
+        #region TIMEOUTS
+
+        #endregion
+
+        #region C/S INTEGRITY
+
+        #endregion
+    }
+}
