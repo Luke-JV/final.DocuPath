bbbdadcea45ec524f4c6aee04275bc229ae50d52
Luke-JV <lukevoigt0407@gmail.com>
2017-08-19 15:29:37 +0200

    [Authorize] filter working!

--- DocuPath/DocuPath/DocuPath/Controllers/HomeController.cs
+++ DocuPath/DocuPath/DocuPath/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace DocuPath.Controllers
         {
             return View();
         }
-
+        [Authorize]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
--- DocuPath/DocuPath/DocuPath/Controllers/ManageController.cs
+++ DocuPath/DocuPath/DocuPath/Controllers/ManageController.cs
@@ -66,11 +66,11 @@ namespace DocuPath.Controllers
             var userId = User.Identity.GetUserId<int>();
             var model = new IndexViewModel
             {
-                HasPassword = HasPassword(),
-                PhoneNumber = await UserManager.GetPhoneNumberAsync(userId),
-                TwoFactor = await UserManager.GetTwoFactorEnabledAsync(userId),
-                Logins = await UserManager.GetLoginsAsync(userId),
-                BrowserRemembered = await AuthenticationManager.TwoFactorBrowserRememberedAsync(userId.ToString())
+                //HasPassword = HasPassword(),
+                //PhoneNumber = await UserManager.GetPhoneNumberAsync(userId),
+                //TwoFactor = await UserManager.GetTwoFactorEnabledAsync(userId),
+                //Logins = await UserManager.GetLoginsAsync(userId),
+                //BrowserRemembered = await AuthenticationManager.TwoFactorBrowserRememberedAsync(userId.ToString())
             };
             return View(model);
         }
