113cc09fe519bfde3c36542570e7e13f47e30a4a
Ruco Pretorius <u12156613@tuks.co.za>
2017-10-11 04:39:20 +0200

    Holy Crap - INSIGHT & HAPTIC is a TON of work!
    Too much to handle...

--- DocuPath/DocuPath/DocuPath/Models/DPViewModels/ReportingViewModel.cs
+++ DocuPath/DocuPath/DocuPath/Models/DPViewModels/ReportingViewModel.cs
@@ -20,15 +20,14 @@ namespace DocuPath.Models.DPViewModels
     {
         [Required]
         public int reportToGenerate { get; set; }
-        [Required]
-        public int reportTimeframe { get; set; }
+        public Nullable<int> reportTimeframe { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]        
+        public Nullable<DateTime> reportDateFrom { get; set; }
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-        public DateTime reportDateFrom { get; set; }
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-        public DateTime reportDateTo { get; set; }
-        public int reportUsersSelector { get; set; }
-        public int reportActivitiesSelector { get; set; }
-        public int reportSuperuserSelector { get; set; }
+        public Nullable<DateTime> reportDateTo { get; set; }
+        public Nullable<int> reportUsersSelector { get; set; }
+        public Nullable<int> reportActivitiesSelector { get; set; }
+        public Nullable<int> reportSuperuserSelector { get; set; }
     }
     public class ReportKVP
     {
@@ -51,6 +50,120 @@ namespace DocuPath.Models.DPViewModels
     {
         public int aId { get; set; }
         public string aDesc { get; set; }
+    }
+
+    public class InsightCODDataset
+    {
+        public string dsTimeframeToString { get; set; }
+        public string dsGeneratedBy { get; set; }
+        public int dsNumEntries { get; set; }
+        public string dsSortedBy { get; set; }
+        public string dsGroupedBy { get; set; }
+        public List<FreqKVP> dsCOD { get; set; }
+        public string dsChartData { get; set; }
+        public string dsChartLabels { get; set; }
+    }
+
+    public class FreqKVP
+    {
+        public int FreqID { get; set; }
+        public string FreqDesc { get; set; }
+        public int FreqCount { get; set; }
+        public string FreqLastClosedDate { get; set; }
+    }
+
+    public class InsightMODDataset
+    {
+        public string dsTimeframeToString { get; set; }
+        public string dsGeneratedBy { get; set; }
+        public int dsNumEntries { get; set; }
+        public string dsSortedBy { get; set; }
+        public string dsGroupedBy { get; set; }
+        public List<FreqKVP> dsMOD { get; set; }
+        public string dsChartData { get; set; }
+        public string dsChartLabels { get; set; }
+    }
+
+    public class InsightSOIDataset
+    {
+        public string dsTimeframeToString { get; set; }
+        public string dsGeneratedBy { get; set; }
+        public int dsNumEntries { get; set; }
+        public string dsSortedBy { get; set; }
+        public string dsGroupedBy { get; set; }
+        public List<FreqKVP> dsSOI { get; set; }
+        public string dsChartData { get; set; }
+        public string dsChartLabels { get; set; }
+    }
+
+    public class InsightOSRDataset
+    {
+        public string dsTimeframeToString { get; set; }
+        public string dsGeneratedBy { get; set; }
+        public int dsNumEntries { get; set; }
+        public string dsSortedBy { get; set; }
+        public string dsGroupedBy { get; set; }
+        public List<OSRKVP> dsOSR { get; set; }
+        public string dsChartData { get; set; }
+        public string dsChartLabels { get; set; }
+    }
+
+    public class OSRKVP
+    {
+        public int SRid { get; set; }
+        public string SPCompanyName { get; set; }
+        public string SRAddedBy { get; set; }
+        public string DRNum { get; set; }
+        public string DateAdded { get; set; }
+        public int DaysOpen { get; set; }
+    }
+
+    public class InsightNewMediaDataset
+    {
+        public string dsTimeframeToString { get; set; }
+        public string dsGeneratedBy { get; set; }
+        public int dsNumEntries { get; set; }
+        public string dsSortedBy { get; set; }
+        public string dsGroupedBy { get; set; }
+        public List<MEDIA> dsNewMedia { get; set; }
+        public string dsChartData { get; set; }
+        public string dsChartLabels { get; set; }
+    }
+
+    public class InsightSUODataset
+    {
+        public string dsTimeframeToString { get; set; }
+        public string dsGeneratedBy { get; set; }
+        public int dsNumEntries { get; set; }
+        public string dsSortedBy { get; set; }
+        public string dsGroupedBy { get; set; }
+        public List<AUDIT_LOG> dsSUO { get; set; }
+        public string dsChartData { get; set; }
+        public string dsChartLabels { get; set; }
+    }
+
+    public class InsightCaseDurationDataset
+    {
+        public string dsTimeframeToString { get; set; }
+        public string dsGeneratedBy { get; set; }
+        public int dsNumEntries { get; set; }
+        public string dsSortedBy { get; set; }
+        public string dsGroupedBy { get; set; }
+        public List<FORENSIC_CASE> dsCaseDuration { get; set; }
+        public string dsChartData { get; set; }
+        public string dsChartLabels { get; set; }
+    }
+
+    public class InsightUARDataset
+    {
+        public string dsTimeframeToString { get; set; }
+        public string dsGeneratedBy { get; set; }
+        public int dsNumEntries { get; set; }
+        public string dsSortedBy { get; set; }
+        public string dsGroupedBy { get; set; }
+        public List<AUDIT_LOG> dsUAR { get; set; }
+        public string dsChartData { get; set; }
+        public string dsChartLabels { get; set; }
     }
     //public class ReportTimeframeRange
     //{
